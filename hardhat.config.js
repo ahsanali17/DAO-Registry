79545a33c1a40f1370bdf8032f827b4a6f0cfcea
ahsanali17 <ahsantime1@gmail.com>
2022-06-18 19:55:06 -0400

    Basic changes to the hardhat config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
-
+require('dotenv').config();
 // The next line is part of the sample project, you don't need it in your
 // project. It imports a Hardhat task definition, that can be used for
 // testing the frontend.
@@ -7,10 +7,17 @@ require("./tasks/faucet");
 
 // If you are using MetaMask, be sure to change the chainId to 1337
 module.exports = {
-  solidity: "0.7.3",
+  solidity: "0.8.4",
   networks: {
     hardhat: {
       chainId: 31337
+    },
+    rinkeby: {
+      url: process.env.RINKEBY_URL,
+      accounts: [process.env.RINKEBY_PRIVATE_KEY]
     }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY
   }
 };
